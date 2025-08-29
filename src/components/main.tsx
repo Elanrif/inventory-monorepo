1ce8f766b5f75ef5830d52c2b11505bf5be71c63
Elanrif <saidbacoelanrif@gmail.com>
2025-08-29 17:30:42 +0100

    feat: first commit

---
+++ src/components/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="flex flex-col mt-5 gap-10 items-center bg-blue-300 py-24 px-10">
+      <h1 className="text-5xl text-center w-5xl">Créez une boutique en ligne et développez votre activité eCommerce</h1>
+      <button className="px-10 py-4 bg-black text-white rounded-full text-lg">Créer une boutique en ligne</button>
+      <p>Essayez Wix, Aucune carte de crédit n&apos;est requise</p>
+    </div>
+  );
+}
