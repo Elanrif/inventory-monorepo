3b77b9cb73e587da1664698057f3d7764bc6a871
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-09-24 15:28:15 +0100

    feat: feat/sidebar-07-second

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,110 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  Command,
+  Home,
+  Settings,
+  ShoppingBag,
+  Store,
+  Users,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { NavSecondary } from "./nav-secondary"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "Jhon Doe",
+    email: "jhondoe@gmail.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Boutique en ligne",
+      logo: Store,
+      plan: "Vente en ligne",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Ma boutique",
+      url: "#",
+      icon: ShoppingBag,
+      isActive: true,
+      items: [
+        {
+          title: "Catégories",
+          url: "#",
+        },
+        {
+          title: "Produit",
+          url: "#",
+        },
+      ],
+    },
+  ],
+    navSecondary2: [
+    {
+      title: "Clients",
+      url: "#",
+      icon: Users,
+    }
+  ],
+      navSecondary1: [
+    {
+      title: "Acceuil",
+      url: "#",
+      icon: Home,
+    }
+  ],
+  projects: [
+    {
+      name: "Paramètres",
+      url: "#",
+      icon: Settings,
+    }
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavSecondary items={data.navSecondary1}/>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary2}/>
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
