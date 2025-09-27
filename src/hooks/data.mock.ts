863adebffbac73497a7ec6ddc052b6f5e412acd4
Elanrif <saidbacoelanrif@gmail.com>
2025-09-27 20:18:19 +0100

    fix: delete data mock

--- src/hooks/data.mock.ts
+++
@@ -1,7 +0,0 @@
-export const userDataMock = {
-  id: "1",
-  name: "Jean Dupont",
-  email: "jean.dupont@example.com",
-  phone: "+33 1 23 45 67 89",
-  address: "123 Rue de la Paix, 75001 Paris, France",
-};
